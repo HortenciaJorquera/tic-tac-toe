595138152c3697f8963bfcc4a15c6a0ba1f4d8e2
Hortencia Jorquera <hortencia.pj@gmail.com>
2021-02-15 15:15:58 +0100

    style board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Square from './Square'
 
+const style = {
+    border: '4px solid darkblue',
+    borderRadius: '10px',
+    width: '250px',
+    height: '250px',
+    margin: '0 auto',
+    display: 'grid',
+    gridTemplate: 'repeat(3, 1fr) / repeat(3, 1fr)'
+};
+
 export default function Board({squares,onClick}) {
     return (
         //squares.map((square,i)=>(<Square value={i} onClick={()=>onClick('value')}/>))
-        <div>
+        <div style={style}>
             <Square value={1} onClick={()=>onClick('value')}/>
             <Square value={2} onClick={()=>onClick('value')}/>
             <Square value={3} onClick={()=>onClick('value')}/>
