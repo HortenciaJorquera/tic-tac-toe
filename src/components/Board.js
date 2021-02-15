b2785670e9ef564ff6396e4328f9f1a5e759d7f9
Hortencia Jorquera <hortencia.pj@gmail.com>
2021-02-15 15:11:29 +0100

    style square component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Square from './Square'
 
-export default function Board() {
+export default function Board({squares,onClick}) {
     return (
+        //squares.map((square,i)=>(<Square value={i} onClick={()=>onClick('value')}/>))
         <div>
-            board
+            <Square value={1} onClick={()=>onClick('value')}/>
+            <Square value={2} onClick={()=>onClick('value')}/>
+            <Square value={3} onClick={()=>onClick('value')}/>
+            <Square value={4} onClick={()=>onClick('value')}/>
+            <Square value={5} onClick={()=>onClick('value')}/>
+            <Square value={6} onClick={()=>onClick('value')}/>
+            <Square value={7} onClick={()=>onClick('value')}/>
+            <Square value={8} onClick={()=>onClick('value')}/>
+            <Square value={9} onClick={()=>onClick('value')}/>
         </div>
     )
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Board from './Board'
 
 export default function Game() {
+    function handleClick(value){
+        console.log(value)
+    }
+
+    function jumpTo(){
+
+    }
+
+    function renderMoves(){
+
+    }
+
     return (
         <div>
-            game
+            <Board squares={new Array(9)} onClick={handleClick}/>
         </div>
     )
 }
